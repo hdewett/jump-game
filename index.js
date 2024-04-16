e7727f29362d8b389932a1a8e311d25d7d2d6e3b
hdewett <h.dewett@outlook.com>
2024-04-15 21:44:38 -0400

    add player code

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+import Player from "./Player.js";
+import Ground from "./Ground.js";
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const GAME_SPEED_START = 0.75;
+const GAME_SPEED_INCREMENT = 0.00001;
+
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 200;
+const PLAYER_WIDTH = 88/1.5;
+const PLAYER_HEIGHT = 94/1.5;
+const MAX_JUMP_HEIGHT = GAME_HEIGHT;
+const MIN_JUMP_HEIGHT = 150;
+const GROUND_WIDTH = 2400;
+const GROUND_HEIGHT = 24;
+const GROUND_AND_CACTUS_SPEED = 0.5;
+
+//Game objects
+let player = null;
+let ground = null;
 
 let scaleRatio = null;
 let previousTime = null;
+let gameSpeed = GAME_SPEED_START;
+
+function createSprites() {
+  const playerWidthInGame = PLAYER_WIDTH * scaleRatio;
+  const playerHeightInGame = PLAYER_HEIGHT * scaleRatio;
+  const minJumpHeightInGame = MIN_JUMP_HEIGHT * scaleRatio;
+  const maxJumpHeightInGame = MAX_JUMP_HEIGHT * scaleRatio;
+
+  const groundWidthInGame = GROUND_WIDTH * scaleRatio;
+  const groundHeightInGame = GROUND_HEIGHT * scaleRatio;
+
+  player = new Player(ctx, playerWidthInGame, playerHeightInGame, minJumpHeightInGame, maxJumpHeightInGame, scaleRatio);
+
+  ground = new Ground(ctx, groundWidthInGame, groundHeightInGame,GROUND_AND_CACTUS_SPEED, scaleRatio);
+}
 
 function setScreen(){
   scaleRatio = getScaleRatio();
   canvas.width = GAME_WIDTH * scaleRatio;
   canvas.height = GAME_HEIGHT * scaleRatio;
+  createSprites();
 }
 
 setScreen();
@@ -55,8 +88,17 @@ function gameLoop(currentTime) {
 
   const frameTimeDelta = currentTime - previousTime;
   previousTime = currentTime;
-  console.log(frameTimeDelta);
+
   clearScreen();
+
+  //update game objects
+  ground.update(gameSpeed, frameTimeDelta);
+
+  //draw game objects
+  ground.draw();
+  player.draw();
+
+
   requestAnimationFrame(gameLoop);
 }
 
